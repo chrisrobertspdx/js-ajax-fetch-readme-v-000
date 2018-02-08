0a7f5290dcd8f644a2cf1ccf07eadb7c88b467d7
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-08 15:36:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '705ca4a054758071adb25612a252b99590627fac'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
